9ef267b2c5df6cda9bc53785efe2f0f094619a2b
jazzIron <dlwocjf90@gmail.com>
2022-01-14 13:49:38 +0900

    [feat] plugin setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process"); // command 명령어 사용가능
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: "development",
   entry: {
@@ -12,17 +18,61 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ], // 순서 중요함, 뒤에서 부터 실행
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "url-loader",
         options: {
-          publicPath: "../dist/",
-          name: "[name].[ext]?[hash]",
-          limit: 20000, // 2kb
+          publicPath: "../dist/", // live server 사용시 path를 못찾음 ./ => ../
+          name: "[name].[ext]?[hash]", // hash 처리(캐시)
+          limit: 20000, // 2kb 최대
         },
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+      Build Date: ${new Date().toLocaleString()}
+      Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")},
+      Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    // 환경 변수 설정 (env)
+    new webpack.DefinePlugin({
+      TWO: JSON.stringify("1+1"),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+      },
+      minify:
+        // 디버깅쉽게 하기위해 production 환경에서만 설정
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석제거
+            }
+          : false,
+    }),
+    // dist Folder delete
+    new CleanWebpackPlugin({}),
+    // javascript css 뽑기(개발환경에는 필요가없음)
+    // loader 설정이 추가로 필요함
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "[name].css",
+          }),
+        ]
+      : []),
+  ],
 };
